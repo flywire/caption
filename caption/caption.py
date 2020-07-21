cb41ee2ade134b78da54536798be83b22a14f1d0
flywire <flywire0@gmail.com>
2020-07-21 18:52:59 +1000

    Restore content settings functionality

--- caption/caption.py
+++ caption/caption.py
@@ -12,84 +12,6 @@ from xml.etree import ElementTree
 
 
 class captionTreeprocessor(Treeprocessor):
-
-    @staticmethod
-    def matchChildren(par):
-        a = None
-        img = par.find("./img")
-        if img is None:
-            a = par.find("./a")
-            if a is not None:
-                img = a.find("./img")
-                if img is None:
-                    a = None
-        return (img, a)
-
-    def buildContentElement(self, par, Type):
-        attrib = par.attrib
-        par.clear()
-        par.tag = Type.content_tag
-        for k, v in attrib.items():
-            par.set(k, v)
-        par.set("id", "_{}-{}".format(Type.name, Type.caption_number))
-        par.text = "\n"
-        par.tail = "\n"
-
-    def buildCaptionElement(self, par, title, Type):
-        caption = ElementTree.SubElement(par, Type.caption_tag)
-        if Type.numbering:
-            caption_prefixSpan = ElementTree.SubElement(caption, Type.prefix_tag)
-            caption_prefixSpan.text = "{}&nbsp;{}:".format(Type.caption_prefix, Type.caption_number)
-            caption_prefixSpan.tail = " {}".format(title)
-        else:
-            caption.text = title
-        caption.tail = "\n"
-
-    def run(self, root):
-        Figure = Content(
-            top_caption = False,
-            content_tag = "figure",
-            linked_process = "strip_title")
-        Table = Content(
-            name = "table",
-            content_tag = "table",
-            caption_tag = "caption",
-            linked_process = "line_2_caption")
-        Listing = Content(
-            name = "listing")
-
-        for par in root.findall("./p"):
-            if par.text and par.text.startswith("Table: "):
-                Type = Table
-                title = par.text[len("Table: "):]
-            elif par.text and par.text.startswith("Listing: "):
-                Type = Listing
-                title = par.text[len("Listing: "):]
-            else:
-                img, a = captionTreeprocessor.matchChildren(par)
-                if img is None:
-                    continue
-                Type = Figure
-
-            Type.caption_number += 1
-            self.buildContentElement(par, Type)
-
-            if Type.name == "figure":
-                if a is not None:
-                    a.tail = "\n"
-                    par.append(a)
-                else:
-                    if img.tail is None:
-                        img.tail = "\n"
-                    else:
-                        img.tail += "\n"
-                    par.append(img)
-                title = img.get("title")
-
-            self.buildCaptionElement(par, title, Type)
-
-
-class Content(captionTreeprocessor):
     def __init__(
         self,
 		name = "figure",
@@ -133,14 +55,107 @@ class Content(captionTreeprocessor):
         else:
            self.id = "_{}-".format(name)
 
+    @staticmethod
+    def matchChildren(par):
+        a = None
+        img = par.find("./img")
+        if img is None:
+            a = par.find("./a")
+            if a is not None:
+                img = a.find("./img")
+                if img is None:
+                    a = None
+        return (img, a)
+
+    def buildContentElement(self, par, Type):
+        attrib = par.attrib
+        par.clear()
+        par.tag = Type.content_tag
+        for k, v in attrib.items():
+            par.set(k, v)
+        par.set("id", "_{}-{}".format(Type.name, Type.caption_number))
+        par.text = "\n"
+        par.tail = "\n"
+
+    def buildCaptionElement(self, par, title, Type):
+        caption = ElementTree.SubElement(par, Type.caption_tag)
+        if Type.numbering:
+            caption_prefixSpan = ElementTree.SubElement(caption, Type.prefix_tag)
+            caption_prefixSpan.text = "{}&nbsp;{}:".format(Type.caption_prefix, Type.caption_number)
+            caption_prefixSpan.tail = " {}".format(title)
+        else:
+            caption.text = title
+        caption.tail = "\n"
+
+    def run(self, root):
+        Figure = captionTreeprocessor(
+            top_caption = False,
+            content_tag = "figure",
+            linked_process = "strip_title")
+        Table = captionTreeprocessor(
+            name = "table",
+            content_tag = "table",
+            caption_tag = "caption",
+            linked_process = "line_2_caption")
+        Listing = captionTreeprocessor(
+            name = "listing")
+
+        for par in root.findall("./p"):
+            if par.text and par.text.startswith("Table: "):
+                Type = Table
+                title = par.text[len("Table: "):]
+            elif par.text and par.text.startswith("Listing: "):
+                Type = Listing
+                title = par.text[len("Listing: "):]
+            else:
+                img, a = captionTreeprocessor.matchChildren(par)
+                if img is None:
+                    continue
+                Type = Figure
+
+            Type.caption_number += 1
+            self.buildContentElement(par, Type)
+
+            if Type.name == "figure":
+                if a is not None:
+                    a.tail = "\n"
+                    par.append(a)
+                else:
+                    if img.tail is None:
+                        img.tail = "\n"
+                    else:
+                        img.tail += "\n"
+                    par.append(img)
+                title = img.get("title")
+
+            self.buildCaptionElement(par, title, Type)
+
 
 class captionExtension(Extension):
     def __init__(self, **kwargs):
-        self.config = {'hosts' : [[], 'List of approved hosts']}
+        self.config = {
+            "caption_prefix" : ["Figure", "The text to show in front of the image caption."],
+            "numbering" : [True, "Add the caption number to the prefix."],
+            "caption_prefix_class" : ["", "CSS class to add to the caption prefix <span /> element."],
+            "caption_class" : ["", "CSS class to add to the caption element."],
+            "content_class" : ["", "CSS class to add to the content element."],
+#            "stripTitle" : [False, "Strip the title from the <img />."],
+        }
         super(captionExtension, self).__init__(**kwargs)
 
     def extendMarkdown(self, md):
-        md.treeprocessors.register(captionTreeprocessor(md), 'captionTreeprocessor', 8)
+        md.treeprocessors.register(
+            captionTreeprocessor(
+                md,
+                caption_prefix=self.getConfig("caption_prefix"),
+                numbering=self.getConfig("numbering"),
+                caption_prefix_class=self.getConfig("caption_prefix_class"),
+                caption_class=self.getConfig("caption_class"),
+                content_class=self.getConfig("content_class"),
+ #               stripTitle=self.getConfig("stripTitle"),
+            ),
+            "captiontreeprocessor",
+            8)
 
 def makeExtension(**kwargs):
     return captionExtension(**kwargs)
