fd90eea9edc3dcd5abde970650daf875d500c554
Andrea Ghensi <aghensi@systra.com>
2023-01-26 10:15:26 +0100

    build: setup github actions

--- caption/__init__.py
+++ caption/__init__.py
@@ -3,4 +3,7 @@
 # Copyright (c) 2020 flywire
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
+
 from .caption import makeExtension, CaptionExtension
+
+__all__ = ['makeExtension', 'CaptionExtension']
