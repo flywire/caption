c4f2dd0d4c24ce231cdb0bf666bfcad3c60c2670
Andrea Ghensi <andrea.ghensi@gmail.com>
2023-01-29 17:25:47 +0100

    tests: table caption tests

--- test/test_table_caption.py
+++ test/test_table_caption.py
@@ -36,8 +36,7 @@ Nothing should change here whilst using caption."""
     assert out_string == expected_string
 
 
-def test_simple_table():
-    in_string = """\
+BASE_MD_TABLE = """\
 Table: Example with heading, two columns and a row
 
 | Syntax      | Description |
@@ -45,58 +44,91 @@ Table: Example with heading, two columns and a row
 | Header      | Title       |
 | Paragraph   | Text        |
 """
+
+
+TABLE_INNER_CONTENT = """<thead>
+<tr>
+<th>Syntax</th>
+<th>Description</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>Header</td>
+<td>Title</td>
+</tr>
+<tr>
+<td>Paragraph</td>
+<td>Text</td>
+</tr>
+</tbody>"""
+
+
+def test_defaults():
     expected_string = """\
 <table id="_table-1">
 <caption><span>Table&nbsp;1:</span> Example with heading, two columns and a row</caption>
-<thead>
-<tr>
-<th>Syntax</th>
-<th>Description</th>
-</tr>
-</thead>
-<tbody>
-<tr>
-<td>Header</td>
-<td>Title</td>
-</tr>
-<tr>
-<td>Paragraph</td>
-<td>Text</td>
-</tr>
-</tbody>
-</table>"""
-    out_string = markdown.markdown(in_string, extensions=["tables", TableCaptionExtension()])
+{}
+</table>""".format(TABLE_INNER_CONTENT)
+    out_string = markdown.markdown(BASE_MD_TABLE, extensions=["tables", TableCaptionExtension()])
     assert out_string == expected_string
 
 
-def test_simple_table_bottom_caption():
-    in_string = """\
-Table: Example with heading, two columns and a row
-
-| Syntax      | Description |
-| ----------- | ----------- |
-| Header      | Title       |
-| Paragraph   | Text        |
-"""
+def test_bottom_caption():
     expected_string = """\
 <table id="_table-1">
 <caption style="caption-side:bottom"><span>Table&nbsp;1:</span> Example with heading, two columns and a row</caption>
-<thead>
-<tr>
-<th>Syntax</th>
-<th>Description</th>
-</tr>
-</thead>
-<tbody>
-<tr>
-<td>Header</td>
-<td>Title</td>
-</tr>
-<tr>
-<td>Paragraph</td>
-<td>Text</td>
-</tr>
-</tbody>
-</table>"""
-    out_string = markdown.markdown(in_string, extensions=["tables", TableCaptionExtension(caption_top=False)])
+{}
+</table>""".format(TABLE_INNER_CONTENT)
+    out_string = markdown.markdown(BASE_MD_TABLE, extensions=["tables", TableCaptionExtension(caption_top=False)])
+    assert out_string == expected_string
+
+
+def test_content_class():
+    expected_string = """\
+<table class="testclass" id="_table-1">
+<caption><span>Table&nbsp;1:</span> Example with heading, two columns and a row</caption>
+{}
+</table>""".format(TABLE_INNER_CONTENT)
+    out_string = markdown.markdown(BASE_MD_TABLE, extensions=["tables", TableCaptionExtension(content_class="testclass")])
+    assert out_string == expected_string
+
+
+def test_caption_class():
+    expected_string = """\
+<table id="_table-1">
+<caption class="testclass"><span>Table&nbsp;1:</span> Example with heading, two columns and a row</caption>
+{}
+</table>""".format(TABLE_INNER_CONTENT)
+    out_string = markdown.markdown(BASE_MD_TABLE, extensions=["tables", TableCaptionExtension(caption_class="testclass")])
+    assert out_string == expected_string
+
+
+def test_no_numbering():
+    expected_string = """\
+<table id="_table-1">
+<caption>Example with heading, two columns and a row</caption>
+{}
+</table>""".format(TABLE_INNER_CONTENT)
+    out_string = markdown.markdown(BASE_MD_TABLE, extensions=["tables", TableCaptionExtension(numbering=False)])
+    assert out_string == expected_string
+
+
+def test_caption_prefix_class():
+    expected_string = """\
+<table id="_table-1">
+<caption><span class="testclass">Table&nbsp;1:</span> Example with heading, two columns and a row</caption>
+{}
+</table>""".format(TABLE_INNER_CONTENT)
+    out_string = markdown.markdown(BASE_MD_TABLE, extensions=["tables", TableCaptionExtension(caption_prefix_class="testclass")])
+    assert out_string == expected_string
+
+
+def test_caption_prefix():
+    expected_string = """\
+<table id="_table-1">
+<caption><span>Tabula&nbsp;1:</span> Example with heading, two columns and a row</caption>
+{}
+</table>""".format(TABLE_INNER_CONTENT)
+    out_string = markdown.markdown(BASE_MD_TABLE, extensions=["tables", TableCaptionExtension(caption_prefix="Tabula")])
     assert out_string == expected_string
