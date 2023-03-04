16b25938e633338cbc1cdbd16e8058e97ead327c
Hendrik Polczynski <hendrikpolczyn@gmail.com>
2023-03-04 22:50:38 +0100

    fix listing unit test (the class was previously supplied through the tag, this produced wrong html syntax (class in closing tag), and prevented applying a custom class)

--- test/test_listing_caption.py
+++ test/test_listing_caption.py
@@ -15,8 +15,8 @@ def test_listing():
     in_string = """\
 Listing: Simple listing test"""
     expected_string = """\
-<div class=listing id="_listing-1">
+<div class="listing" id="_listing-1">
 <figcaption><span>Listing&nbsp;1:</span> Simple listing test</figcaption>
-</div class=listing>"""
+</div>"""
     out_string = markdown.markdown(in_string, extensions=[CaptionExtension()])
     assert out_string == expected_string
